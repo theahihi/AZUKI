34c9bdf63692362413e3eb5248f8eee5cfc546d6
theahihi <theducnguyen17@gmail.com>
2024-06-24 16:17:27 +0700

    16:1724062024

--- js/trangchu.js
+++ js/trangchu.js
@@ -48,13 +48,7 @@ document.getElementById("bogach").addEventListener("click", function (event) {
 document.getElementById("search-button").addEventListener("click", function () {
   let query = document.getElementById("search-input").value.toLowerCase();
 
-  // Kiểm tra từ khóa tìm kiếm
-  if (query.includes("áo điều hoà")) {
-    window.location.href = "../Quyen/ao_dieu_hoa.html"; // Đường dẫn đến trang áo điều hoà
-  } else {
-    alert("Không tìm thấy sản phẩm."); // Thông báo nếu không tìm thấy sản phẩm
-  }
-});
+  
 
 document.addEventListener("DOMContentLoaded", function () {
   let lastScrollTop = 0;
