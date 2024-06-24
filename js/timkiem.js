c255f61c6e35de4991d29392efac290e34bf9c50
theahihi <theducnguyen17@gmail.com>
2024-06-24 16:08:14 +0700

    24062024

--- js/timkiem.js
+++ js/timkiem.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // Kiểm tra từ khóa tìm kiếm
       if (query.includes("áo điều hoà")) {
-        window.location.href = "../Quyen/ao_dieu_hoa.html"; // Đường dẫn đến trang áo điều hoà
+        window.location.href =
+          "https://theahihi.github.io/AZUKI/Quyen/ao_dieu_hoa.html"; // Đường dẫn đến trang áo điều hoà
       } else {
         alert("Không tìm thấy sản phẩm."); // Thông báo nếu không tìm thấy sản phẩm
       }
