776933b0eeb2b499186d5c982456bd33ed420915
theahihi <theducnguyen17@gmail.com>
2024-07-06 15:24:37 +0700

    update

---
+++ js/tudongfit.js
@@ -0,0 +1,14 @@
+function setZoom() {
+  const minWidth = 900; // Độ rộng tối thiểu mà bạn muốn trang web có
+  const screenWidth = window.innerWidth;
+  let zoomLevel = 1; // Mặc định là 100%
+
+  if (screenWidth < minWidth) {
+    zoomLevel = screenWidth / minWidth;
+  }
+
+  document.body.style.zoom = zoomLevel;
+}
+
+window.onload = setZoom;
+window.onresize = setZoom;
