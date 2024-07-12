8ba2cc1439dc700cf2d320eb97b03afee1ba3cc8
theahihi <theducnguyen17@gmail.com>
2024-07-12 07:20:09 +0700

    update

--- hồng/js/dangnhap.js
+++ hồng/js/dangnhap.js
@@ -1,16 +1,20 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
     // Lấy giá trị từ các trường nhập liệu
-    var username = document.querySelector('input[name="username"]').value.trim();
+    var username = document
+      .querySelector('input[name="username"]')
+      .value.trim();
     var password = document.querySelector('input[name="pass"]').value.trim();
-    
+
     // Kiểm tra xem các trường có được điền đầy đủ hay không
-    if (username === '' || password === '') {
-        document.getElementById('errorMessage').style.display = 'block';
+    if (username === "" || password === "") {
+      document.getElementById("errorMessage").style.display = "block";
     } else {
-        document.getElementById('errorMessage').style.display = 'none';
-        // Chuyển hướng tới trang chủ sau khi đăng nhập thành công
-        window.location.href = "../../index.html"; 
+      document.getElementById("errorMessage").style.display = "none";
+      // Chuyển hướng tới trang chủ sau khi đăng nhập thành công
+      window.location.href = "https://theahihi.github.io/AZUKI/";
     }
-});
+  });
